6b3f4e994ba44b188e31bb4204799e160be5ef69
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-02-04 10:22:55 -0300

    added buttons order

--- javascript.js
+++ javascript.js
@@ -2,34 +2,46 @@
 function add(a,b) {    
 let num1 = a;
 let num2 = b;
-let operand = '+';
     return num1 + num2;
 }
 
 function multiply(a,b){
 let num1 = a;
 let num2 = b;
-let operand = '*';
     return num1 * num2;
 }
 
 function subtract(a,b){
 let num1 = a;
 let num2 = b;
-let operand = '-';
     return num1 - num2;
 }
 
 function divide(a,b){
 let num1 = a;
 let num2 = b;
-let operand = '/';
 if (num1 === 0 || num2 === 0){
     return console.log('Cannot Divide by 0');
 }
     return num1/num2;
 }
 
-function operate() {
+function operate(a,b, operator) {
+ switch (operator){
+    case "+":
+        return add(a,b);
+    case "-":
+        return subtract(a,b);
+    case "*":
+        return multiply(a,b);
+    case "/":
+        return divide(a,b);
+ }   
+};
 
-}
+
+
+const atual = document.querySelector('.atual')
+atual.textContent = "asd";
+const stored = document.querySelector('.stored')
+stored.textContent = "ads";
