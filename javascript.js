1932032fac02ca224b8c3797e9cb1d035ac289a5
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-02-04 11:48:50 -0300

    added display update, buttons function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+// operations
 function add(a,b) {    
 let num1 = a;
 let num2 = b;
@@ -25,23 +25,64 @@ if (num1 === 0 || num2 === 0){
 }
     return num1/num2;
 }
-
+// switch for operations
 function operate(a,b, operator) {
  switch (operator){
     case "+":
         return add(a,b);
+        break;
     case "-":
         return subtract(a,b);
+        break;
     case "*":
         return multiply(a,b);
+        break;
     case "/":
         return divide(a,b);
+        break;
  }   
 };
 
-
-
+// DOM ELEMENTS
+const numberKeys = document.querySelectorAll('.number')
+const operations = document.querySelectorAll('.operation')
+const clearBtn = document.querySelector('.clear')
+const deleteBtn = document.querySelector('.delete')
 const atual = document.querySelector('.atual')
-atual.textContent = "asd";
 const stored = document.querySelector('.stored')
-stored.textContent = "ads";
+atual.textContent = 0;
+
+
+
+// variables
+
+let storedNum = "";
+let currentNum = "";
+let result = "";
+let operator = "";
+
+
+//update the display
+
+numberKeys.forEach((button)=> {
+    button.addEventListener('click', function (){
+        currentNum += button.innerHTML;
+        atual.textContent = currentNum;
+    })
+})
+
+operations.forEach((button)=> {
+    button.addEventListener('click', function (){
+        storedNum = currentNum;
+        clickedOperator = button.innerHTML;
+        stored.textContent = storedNum + ' ' + clickedOperator;
+        currentNum = "";
+    })
+})
+
+clearBtn.addEventListener('click', function() {
+    storedNum = "";
+    currentNum = "";
+    atual.textContent = 0;
+    stored.textContent = "";
+})
